1daed90b696046548cdc11a8e67251f56b85dd02
Paul Hobbel <paulhobbel@hotmail.com>
2015-01-04 17:26:11 +0100

    Did some rework on the thread

--- app/views/forum/thread.blade.php
+++ app/views/forum/thread.blade.php
@@ -6,55 +6,74 @@
 @stop
 
 @section('content')
-	<ol class="breadcrumb">
-		<li><a href="{{ URL::route('forum-home') }}">Forum</a></li>
-		<li><a href="{{ URL::route('forum-category', $thread->category_id) }}">{{ $thread->category->title }}</a></li>
-		<li class="active">{{ $thread->title }}</li>
-		@if(Auth::check() && Auth::user()->isAdmin() || Auth::check() && Auth::user()->id == $thread->author_id)
-			<div class="dropdown pull-right">
-				<button id="options-menu" type="button" class="btn btn-default btn-xs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options <span class="caret"></span></button>
-				<ul class="dropdown-menu" role="menu" aria-labelledby="options-menu">
-					<li><a href="{{ URL::route('forum-edit-thread', $thread->id) }}">Edit Thread</a></li>
-					<li><a href="#" data-toggle="modal" data-target="#delete_modal" data-backdrop="false">Delete Thread</a></li>
-				</ul>
-			</div>
-		@endif
-	</ol>
+<div class="sixteen wide column">
+	<div class="ui breadcrumb segment" style="width: 100%;">
+		<a href="{{ URL::route('forum-home') }}" class="section">Forum</a>
+		<i class="right chevron icon divider"></i>
+		<a href="{{ URL::route('forum-home') }}" class="section">{{ $thread->group->title }}</a>
+		<i class="right chevron icon divider"></i>
+		<a href="{{ URL::route('forum-category', $thread->category->id) }}" class="section">{{ $thread->category->title }}</a>
+		<i class="right chevron icon divider"></i>
+		<div class="active section">{{ $thread->title }}</div>
+	</div>
+</div>
 
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4>{{ $thread->title }}<br><small>by <strong>{{ $author }}</strong> &raquo; <em>{{ $thread->created_at }}</em></small></h4>
-		</div>
-		<div class="panel-body">
-			{{ BBCode::parse($thread->body) }}
-			@if($thread->updated_at != $thread->created_at)
-			<br><small>Last edited by <strong>{{ $author }}</strong> on <em>{{ $thread->updated_at }}</em></small>
-			@endif
+@if(Auth::check())
+<div class="row">
+	<div class="eight wide column">
+		<a href="#" class="ui vertical animated teal button">
+			<div class="hidden content">Reply</div>
+			<div class="visible content">
+				<i class="reply icon"></i>
+			</div>
+		</a>
+	</div>
+	@if(Auth::user()->isAdmin() || Auth::user()->id == $thread->author_id)
+	<div class="right aligned eight wide column">
+		<div class="ui buttons">
+			<a href="{{ URL::route('forum-edit-thread', $thread->id) }}" class="ui button">Edit</a>
+			<div class="or"></div>
+			<div class="ui negative button">Delete</div>
 		</div>
 	</div>
+	@endif
+</div>
+@endif
 
-	@foreach($thread->comments()->get() as $comment)
-		<div class="panel panel-default">
-			<div class="panel-heading clearfix">
-				<h4 class="pull-left">Re: {{ $comment->thread->title }}<br><small>by <strong>{{ $comment->author->username }}</strong> &raquo; <em>{{ $comment->created_at }}</em></small></h4>
-				@if(Auth::check() && Auth::user()->isAdmin() || Auth::check() && Auth::user()->id == $thread->author_id)
-					<div class="dropdown pull-right">
-						<button id="options-menu" type="button" class="btn btn-default btn-xs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options <span class="caret"></span></button>
-						<ul class="dropdown-menu" role="menu" aria-labelledby="options-menu">
-							<li><a href="{{ URL::route('forum-edit-comment', $comment->id) }}">Edit Comment</a></li>
-							<li><a href="{{ URL::route('forum-delete-comment', $comment->id) }}">Delete Comment</a></li>
-						</ul>
-					</div>
-				@endif
-			</div>
-			<div class="panel-body">
-				{{ BBCode::parse($comment->body) }}
-				@if($comment->updated_at != $comment->created_at)
-				<br><small>Last edited by <strong>{{ $comment->author->username }}</strong> on <em>{{ $comment->updated_at }}</em></small>
-				@endif
-			</div>
-		</div>
-	@endforeach
+<div class="sixteen wide column">
+	<div class="ui top attached segment thread-title">
+		<h4>{{ $thread->title }}</h4><small>by <strong>{{ $author }}</strong> &raquo; <em>{{ $thread->created_at }}</em></small>
+	</div>
+	<div class="ui attached segment">
+		<p>{{ BBCode::parse($thread->body) }}</p>
+		@if($thread->updated_at != $thread->created_at)
+		<div class="ui bottom left attached label">Last edited by {{ $author }} on <em>{{ $thread->updated_at }}</em></div>
+		@endif
+	</div>
+</div>
+
+@foreach($thread->comments()->get() as $comment)
+<div class="sixteen wide column">
+	<div class="ui top attached segment thread-title">
+		<h4>Re: {{ $comment->thread->title }}</h4><small>by <strong>{{ $comment->author->username }}</strong> &raquo; <em>{{ $comment->created_at }}</em></small>
+		@if(Auth::check() && Auth::user()->isAdmin() || Auth::check() && Auth::user()->id == $thread->author_id)
+			<!--div class="dropdown pull-right">
+				<button id="options-menu" type="button" class="btn btn-default btn-xs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options <span class="caret"></span></button>
+				<ul class="dropdown-menu" role="menu" aria-labelledby="options-menu">
+					<li><a href="{{ URL::route('forum-edit-comment', $comment->id) }}">Edit Comment</a></li>
+					<li><a href="{{ URL::route('forum-delete-comment', $comment->id) }}">Delete Comment</a></li>
+				</ul>
+			</div-->
+		@endif
+	</div>
+	<div class="ui attached segment">
+		<p>{{ BBCode::parse($comment->body) }}</p>
+		@if($comment->updated_at != $comment->comment)
+		<div class="ui bottom left attached label">Last edited by {{ $comment->author->username }} on <em>{{ $comment->updated_at }}</em></div>
+		@endif
+	</div>
+</div>
+@endforeach
 
 	@if(Auth::check())
 		<form method="post" action="{{ URL::route('forum-store-comment', $thread->id) }}">
